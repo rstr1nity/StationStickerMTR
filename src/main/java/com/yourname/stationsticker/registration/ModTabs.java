b3cb982b303d866a14693ba148609e3cf94706d4
rstr1nity <aleksandrlednovic@gmail.com>
2026-04-01 03:36:47 +0300

    мб финальная версия

--- src/main/java/com/yourname/stationsticker/registration/ModTabs.java
+++ src/main/java/com/yourname/stationsticker/registration/ModTabs.java
@@ -9,17 +9,18 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModTabs {
-    // Регистрируем творческую вкладку
     public static final DeferredRegister<CreativeModeTab> TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, StationStickerMod.MOD_ID);
 
     public static final RegistryObject<CreativeModeTab> STICKER_TAB = TABS.register("stationsticker_tab",
             () -> CreativeModeTab.builder()
-                    .title(Component.literal("Station Stickers"))
+                    .title(Component.translatable("itemGroup.stationsticker_tab")) // Используйте перевод
                     .icon(() -> new ItemStack(ModBlocks.STATION_STICKER.get()))
                     .displayItems((parameters, output) -> {
-                        // Добавляем наш блок во вкладку
+                        // Добавляем предметы в порядке очереди
                         output.accept(ModBlocks.STATION_STICKER.get());
+                        output.accept(ModBlocks.LINE_SCHEME.get());
                     })
                     .build());
 }
+
